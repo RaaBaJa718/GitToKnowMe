923835c6a8af83f1cea7adab8f7f4d17b7217291
Ruben <ruben.alston646@gmail.com>
2025-06-05 09:56:37 -0400

    Refactor code structure for improved readability and maintainability

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,9 +5,9 @@ export default function Navbar() {
         <nav className="p-4 bg-gray-900 text-white flex justify-between">
             <h1 className="text-lg font-bold">GitToKnowMe</h1>
             <div className="space-x-4">
-                <Link href="/"><a>Home</a></Link>
-                <Link href="/projects"><a>Projects</a></Link>
-                <Link href="/about"><a>About</a></Link>
+                <Link href="/">Home</Link>
+                <Link href="/projects">Projects</Link>
+                <Link href="/about">About</Link>
             </div>
         </nav>
     );
